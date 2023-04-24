a76db01a817483aa83767cf75e6f88463585404c
nachlielEasy <nachliel@easyappshq.com>
2023-04-24 15:46:01 +0300

    import image into endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-
+import profilePic from "../public/Profile.png";
 export default function Home() {
   return (
     <>
       <h1>My Homepage</h1>
       <Image
         id="id_for_universal_embedder_e2e_test"
-        src="/../public/Profile.png"
+        src={profilePic}
         alt="Picture of the author"
         width={500}
         height={500}
