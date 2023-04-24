00869dcd2d30b3b09fa3d5069085f35f3a3573f5
nachlielEasy <nachliel@easyappshq.com>
2023-04-24 15:42:57 +0300

    test commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,22 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-import Image from "next/image";
 
-function Home() {
-  return (
-    <>
-      <h1>My Homepage</h1>
-      <Image
-        src="/me.png"
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
-      <p>Welcome to my homepage!</p>
-    </>
-  );
-}
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
