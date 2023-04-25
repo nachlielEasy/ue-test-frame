9dcaa6e66612e791ecaed9df7e8911a6e505edca
nachlielEasy <nachliel@easyappshq.com>
2023-04-25 13:35:36 +0300

    remove image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,14 @@ import profilePic from "../public/Profile.png";
 export default function Home() {
   return (
     <>
-      <h1>My Homepage</h1>
-      <Image
+      <h1 id="id_for_universal_embedder_e2e_test">My Homepage</h1>
+      {/* <Image
         id="id_for_universal_embedder_e2e_test"
         src={profilePic}
         alt="Picture of the author"
         width={500}
         height={500}
-      />
+      /> */}
       <p>Welcome to my homepage!</p>
     </>
   );
